84d2fd3e7f3105b3eaafc5c6ab102824ee454fe8
wpedrak <woj123@gmail.com>
2019-04-23 09:07:56 +0200

    style

--- list6/2/demo.sql
+++ list6/2/demo.sql
@@ -5,18 +5,19 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 -- column level encryption (simply crypto functions)
 
+DROP TABLE secret_card;
 CREATE TABLE secret_card(
-    own varchar(255),
-    num varchar(255)
+    own VARCHAR(255),
+    num VARCHAR(255)
     );
 
-insert into secret_card (own, num) values
+insert into "secret_card" (own, num) values
 ('ala',PGP_SYM_ENCRYPT('11111','AES_KEY')),
 ('marek',PGP_SYM_ENCRYPT('22222','AES_KEY')),
 ('ola',PGP_SYM_ENCRYPT('33333','AES_KEY'));
 
-select * from secret_card;
+select * from "secret_card";
 
-select own,pgp_sym_decrypt(num::bytea,'AES_KEY') as num from secret_card;
+select own,pgp_sym_decrypt(num::bytea,'AES_KEY') as num from "secret_card";
 
 -- no key management included
